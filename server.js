9d2ee5b999090b6c9bd5513efbf10880f245d945
ViralDemon <1viraldemon@gmail.com>
2025-06-22 17:43:24 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+{
+  "name": "luma-api-proxy",
+  "version": "1.0.0",
+  "description": "API proxy server for Luma Dream Machine API",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0",
+    "cors": "^2.8.5"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
